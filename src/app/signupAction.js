1416f887f3ddd8da793cd5c2ccdf8477de85d978
18mayur <mayu2003shinde@gmail.com>
2025-12-19 13:04:07 +0530

    header origin added

--- src/app/signupAction.js
+++ src/app/signupAction.js
@@ -1,6 +1,10 @@
 "use server";
-const BaseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+import { headers } from "next/headers";
+// const BaseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
 export async function signupAction(prevState, formData) {
+  const headerList = await headers();
+  const origin = headerList.get("origin");
   const name = formData.get("name");
   const email = formData.get("email");
   const password = formData.get("password");
@@ -16,14 +20,14 @@ export async function signupAction(prevState, formData) {
 
   if (role === "Admin") {
     console.log("This a is for the admin routes");
-    const res = await fetch(`${BaseUrl}/api/admins`, {
+    const res = await fetch(`${origin}/api/admins`, {
       method: "POST",
       body: JSON.stringify({ name, email, password, role, team }),
       headers: { "Content-Type": "application/json" },
     });
   } else {
     console.log("This is a for the User routes");
-    const res = await fetch("http://localhost:3000/api/Userdata", {
+    const res = await fetch(`${origin}/api/Userdata`, {
       method: "POST",
       body: JSON.stringify({ name, email, password, role }),
       headers: { "Content-Type": "application/json" },
