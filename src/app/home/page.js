b14686dffef60a65f0ac024769bac75d461ce2c5
18mayur <mayu2003shinde@gmail.com>
2025-12-20 17:57:40 +0530

    home page error?

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -2,7 +2,50 @@ import React from "react";
 import { getUsers } from "@/services/getUsers";
 import "./style.css";
 
-const home = async () => {
+// const home = async () => {
+//   const userList = await getUsers();
+//   console.log("user list ", userList);
+//   return (
+//     <>
+//       {userList.length <= 0 ? (
+//         <div className="container">
+//           <h1 className="text-[3rem] text-white">No user added in DB</h1>
+//         </div>
+//       ) : (
+//         <div class="container">
+//           <table>
+//             <thead>
+//               <tr>
+//                 <th>Sr. NO</th>
+//                 <th>Name</th>
+//                 <th>Email</th>
+//                 <th>Role</th>
+//               </tr>
+//             </thead>
+//             <tbody>
+//               {userList.map((item, index) => {
+//                 return (
+//                   <tr key={index}>
+//                     <td>{index + 1}</td>
+//                     <td>{item.name}</td>
+//                     <td>{item.email}</td>
+//                     <td>{item.role}</td>
+//                   </tr>
+//                 );
+//               })}
+//             </tbody>
+//           </table>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default home;
+
+// import React from 'react'
+
+export default async function home() {
   const userList = await getUsers();
   console.log("user list ", userList);
   return (
@@ -39,6 +82,4 @@ const home = async () => {
       )}
     </>
   );
-};
-
-export default home;
+}
