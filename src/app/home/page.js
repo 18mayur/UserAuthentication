93980ca0f4298a5a811d84f1ca1991c9ad050867
18mayur <mayu2003shinde@gmail.com>
2025-12-22 11:50:28 +0530

    testing home page

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,10 +1,19 @@
-import React from "react";
+// "use client";
+// import { useEffect, useState } from "react";
 import { getUsers } from "@/services/getUsers";
 import "./style.css";
 
 const home = async () => {
+  // const [userList, setUserList] = useState([]);
+  // useEffect(() => {
+  //   const fetchUsers = async () => {
+  //     const user = await getUsers();
+  //     setUserList(user);
+  //     console.log("user list ", userList);
+  //   };
+  //   fetchUsers();
+  // }, [userList]);
   const userList = await getUsers();
-  console.log("user list ", userList);
   return (
     <div class="container">
       <table>
@@ -30,38 +39,6 @@ const home = async () => {
         </tbody>
       </table>
     </div>
-    // <>
-    //   {userList.length <= 0 ? (
-    //     <div className="container">
-    //       <h1 className="text-[3rem] text-white">No user added in DB</h1>
-    //     </div>
-    //   ) : (
-    //     <div class="container">
-    //       <table>
-    //         <thead>
-    //           <tr>
-    //             <th>Sr. NO</th>
-    //             <th>Name</th>
-    //             <th>Email</th>
-    //             <th>Role</th>
-    //           </tr>
-    //         </thead>
-    //         <tbody>
-    //           {userList.map((item, index) => {
-    //             return (
-    //               <tr key={index}>
-    //                 <td>{index + 1}</td>
-    //                 <td>{item.name}</td>
-    //                 <td>{item.email}</td>
-    //                 <td>{item.role}</td>
-    //               </tr>
-    //             );
-    //           })}
-    //         </tbody>
-    //       </table>
-    //     </div>
-    //   )}
-    // </>
   );
 };
 
--- src/services/getUsers.js
+++ src/services/getUsers.js
@@ -1,10 +1,14 @@
 import { connectMongoose } from "@/lib/mogoose";
 import { UserModel } from "@/lib/model/user";
+import { adminModel } from "@/lib/model/adminUser";
 
 export async function getUsers() {
   await connectMongoose();
 
   const users = await UserModel.find().select("-password").lean();
+  const admin = await adminModel.find().select("-password").lean();
 
-  return users;
+  const allUsers = [...users, ...admin];
+
+  return allUsers;
 }
